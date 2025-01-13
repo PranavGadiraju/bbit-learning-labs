ddc50115462cac3534d2f670aec4dcbe9a034143
Pranav Gadiraju <pranavgadiraju@Pranavs-MacBook-Pro.local>
2025-01-13 14:33:16 -0500

    producer class run and tested

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -0,0 +1,26 @@
+import pika
+import os
+from producer_interface import mqProducerInterface
+
+class mqProducer(mqProducerInterface) :
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        self.con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=self.con_params)
+        self.channel = self.connection.channel()
+        self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
+
+    def publishOrder(self, message: str) -> None:
+        self.channel.basic_publish(
+            exchange="Exchange Name",
+            routing_key="Routing Key",
+            body=message,
+            )
+        self.channel.close()
+        self.connection.close()
+        
+                
